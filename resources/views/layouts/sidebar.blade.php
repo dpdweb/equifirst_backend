a8525a6ab26b39976ca5b87379fdcf0efa456b84
jamshed45 <jamshed76@gmail.com>
2025-06-20 03:41:21 +0500

    admin update

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,91 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Main</li>
+
+                <li>
+                    <a href="index" class="waves-effect">
+                        <i class="ti-home"></i>
+                        <span>Starter Page</span>
+                    </a>
+                </li>
+
+              
+                <li class="menu-title">Extras</li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="ti-layout"></i>
+                        <span>Layouts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li>
+                            <a href="#" class="has-arrow">Vertical</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="layouts-light-sidebar">Light Sidebar</a></li>
+                                <li><a href="layouts-compact-sidebar">Compact Sidebar</a></li>
+                                <li><a href="layouts-icon-sidebar">Icon Sidebar</a></li>
+                                <li><a href="layouts-boxed">Boxed Layout</a></li>
+                                <li><a href="layouts-colored-sidebar">Colored Sidebar</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="has-arrow">Horizontal</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="layouts-horizontal">Horizontal</a></li>
+                                <li><a href="layouts-hori-topbar-light">Light Topbar</a></li>
+                                <li><a href="layouts-hori-boxed">Boxed Layout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="ti-archive"></i>
+                        <span> Authentication </span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="pages-login">Login 1</a></li>
+                        <li><a href="pages-login-2">Login 2</a></li>
+                        <li><a href="pages-register">Register 1</a></li>
+                        <li><a href="pages-register-2">Register 2</a></li>
+                        <li><a href="pages-recoverpw">Recover Password 1</a></li>
+                        <li><a href="pages-recoverpw-2">Recover Password 2</a></li>
+                        <li><a href="pages-lock-screen">Lock Screen 1</a></li>
+                        <li><a href="pages-lock-screen-2">Lock Screen 2</a></li>
+                    </ul>
+                </li>
+
+              
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="ti-more"></i>
+                        <span>Multi Level</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li><a href="#">Level 1.1</a></li>
+                        <li><a href="#" class="has-arrow">Level 1.2</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="#">Level 2.1</a></li>
+                                <li><a href="#">Level 2.2</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
